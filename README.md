4a9a6bd4d5d4e115c4cc1938cd51aa2ec9f6d8f1
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-03-24 09:08:04 -0700

    implement the c s and % conversions

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Printf
+# Custom printf function
 
-## 
+This is an implementation of the printf() function in C that supports the %c and %s format specifiers. The function is implemented in the main.c file, and it depends on the `unistd.h` and `stdarg.h` libraries.
+
+## Usage
+
+To use the custom printf() function, you can include the main.h header file in your C program and call the `_printf()` function with a format string and any necessary arguments. For example:
+
+```#include "main.h"
+
+int main(void)
+{
+    _printf("Hello, %s!\n", "world");
+    return 0;
+}```
--- _printf.c
+++ _printf.c
@@ -1,25 +1,65 @@
 #include "main.h"
 
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to write
+ *
+ * Return: 1.
+ */
+
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * _putstr - writes a string to stdout
+ * @str: the string to write
+ *
+ * Return: the number of characters written
+ */
+
+int _putstr(char *str)
+{
+	int i;
+	int len;
+
+	len = 0;
+	i = 0;
+	while (str[i])
+	{
+		len += _putchar(str[i]);
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * _printf - prints formatted output to stdout
+ * @format: the format string to use
+ *
+ * Return: the number of characters printed
+ */
+
 int _printf(const char *format, ...)
 {
 	va_list list;
 	int i = 0;
 	int count = 0;
-	char arg;
 
 	va_start(list, format);
-
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
 			if (format[i] == 'c')
-			{
-				arg = va_arg(list, int);
-				write(1, &arg, 1);
-				count++;
-			}
+				count += _putchar(va_arg(list, int));
+			if (format[i] == 's')
+				count += _putstr(va_arg(list, char *));
+			if (format[i] == '%')
+				count += _putchar(format[i]);
 		}
 		else
 		{
@@ -28,5 +68,5 @@ int _printf(const char *format, ...)
 		}
 		i++;
 	}
-	return count;
+	return (count);
 }
