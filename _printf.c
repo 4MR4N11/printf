6fc01508933496471f2bd134d80a18701d8fe322
AnasBenba <anasbenba123@gmail.com>
2023-03-25 04:25:29 -0700

    fix error

--- _printf.c
+++ _printf.c
@@ -127,6 +127,10 @@ int _printf(const char *format, ...)
 	va_start(list, format);
 	while (format[i])
 	{
+		if (!format || (format[0] == '%' && !format[1]))
+			return (-1);
+		if (format[0] == '%' && format[1] == ' ' && !format[2])
+			return (-1);
 		if (format[i] == '%')
 		{
 			i++;
