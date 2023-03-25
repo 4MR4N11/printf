c9684f773fc5841c6e4cf2816762a4568dbda285
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-03-25 07:20:05 -0700

    Fixing some issues

--- _printf.c
+++ _printf.c
@@ -27,7 +27,7 @@ int _putstr(char *str)
 
 	len = 0;
 	i = 0;
-	while (str[i])
+	while (str && str[i])
 	{
 		len += _putchar(str[i]);
 		i++;
@@ -37,46 +37,42 @@ int _putstr(char *str)
 
 /**
  * _print_number - prints a number to stdout
- * @n: the number to print
+ * @num: the number to print
  *
  * Return: the number of characters printed
  */
 
 int _print_number(int num)
 {
-        int count = 0;
+	int count = 0;
 
-        if (num < 0)
-        {
-                count += _putchar('-');
-                num = -num;
-        }
+	if (num < 0)
+	{
+		count += _putchar('-');
+		num = -num;
+	}
+	if (num == 0)
+	{
+		count += _putchar('0');
+	}
+	else
+	{
+		char num_str[12];
+		int i = 0;
 
-        if (num == 0)
-        {
-                count += _putchar('0');
-        }
-        else
-        {
-                char num_str[12];
-                int i = 0;
-
-                while (num != 0)
-                {
-                        num_str[i] = (num % 10) + '0';
-                        num /= 10;
-                        i++;
-                }
-
-                num_str[i] = '\0';
-
-                for (i = i - 1; i >= 0; i--)
-                {
-                        count += _putchar(num_str[i]);
-                }
-        }
-
-        return (count);
+		while (num != 0)
+		{
+			num_str[i] = (num % 10) + '0';
+			num /= 10;
+			i++;
+		}
+		num_str[i] = '\0';
+		for (i = i - 1; i >= 0; i--)
+		{
+			count += _putchar(num_str[i]);
+		}
+	}
+	return (count);
 }
 
 /**
@@ -125,7 +121,7 @@ int _printf(const char *format, ...)
 	int count = 0;
 
 	va_start(list, format);
-	while (format[i])
+	while (format && format[i])
 	{
 		if (!format || (format[0] == '%' && !format[1]))
 			return (-1);
