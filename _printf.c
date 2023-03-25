53ffb2b072b78acaaacbedd2ede67ddae568feea
AnasBenba <anasbenba123@gmail.com>
2023-03-25 02:46:02 -0700

    Handle the custom conversion specifiers b

--- _printf.c
+++ _printf.c
@@ -79,6 +79,31 @@ int _print_number(int num)
         return (count);
 }
 
+int _print_binary(unsigned int num)
+{
+	int i = 0, j;
+	unsigned int count = 0;
+	unsigned int arr[64];
+
+	if (num == 0)
+	{
+		count += _putchar(0 + '0');
+		return(0);
+	}
+
+	for (; num > 0;)
+	{
+		arr[i++] = num % 2;
+		num /= 2;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		count += _putchar(arr[j] + '0');
+	}
+	return (count);
+}
+
 /**
  * _printf - prints formatted output to stdout
  * @format: the format string to use
@@ -106,6 +131,8 @@ int _printf(const char *format, ...)
 				count += _putchar(format[i]);
 			if (format[i] == 'd' || format[i] == 'i')
 				count += _print_number(va_arg(list, int));
+			if (format[i] == 'b')
+				count += _print_binary(va_arg(list, unsigned int));
 		}
 		else
 		{
