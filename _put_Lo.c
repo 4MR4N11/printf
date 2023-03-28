d4d58df890a29e275a41f9a5c1d26d5f881a9e74
AnasBenba <anasbenba123@gmail.com>
2023-03-28 09:05:55 -0700

    fix _put_Lo.c

--- _put_Lo.c
+++ _put_Lo.c
@@ -9,37 +9,37 @@
 
 int     _put_Lo(unsigned long int nb, flags_t flags)
 {
-        unsigned long int octal[22];
-        int i, j, count;
+	unsigned long int octal[22];
+	int i, j, count;
 
-        i = 0;
-        j = 0;
-        count = 0;
-        if (flags.h == 1)
-                nb = (unsigned short)nb;
-        else if (flags.h == 2)
-                nb = (unsigned char)nb;
-        else if (flags.l == 1)
-                nb = (unsigned long)nb;
-        while (nb > 0)
-        {
-                octal[i] = nb % 8;
-                nb /= 8;
-                i++;
-        }
-        if (i == 0)
-        {
-                _putchar('0');
-                return (1);
-        }
-        j = i - 1;
-        if (flags.hash && octal[j] != 0)
-                count += _putchar('0');
-        while (j >= 0)
-        {
-                _putchar(octal[j] + '0');
-                count++;
-                j--;
-        }
-        return (count);
+	i = 0;
+	j = 0;
+	count = 0;
+	if (flags.h == 1)
+		nb = (unsigned short)nb;
+	else if (flags.h == 2)
+		nb = (unsigned char)nb;
+	else if (flags.l == 1)
+		nb = (unsigned long)nb;
+	while (nb > 0)
+	{
+		octal[i] = nb % 8;
+		nb /= 8;
+		i++;
+	}
+	if (i == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	j = i - 1;
+	if (flags.hash && octal[j] != 0)
+		count += _putchar('0');
+	while (j >= 0)
+	{
+		_putchar(octal[j] + '0');
+		count++;
+		j--;
+	}
+	return (count);
 }
